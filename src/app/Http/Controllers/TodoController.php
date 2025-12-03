1aa6759ff8d1e64ea905821d1023cd6f5ccdcdb0
ikemi yuki <miilyu237@outlook.jp>
2025-12-03 10:42:11 +0900

    追加機能の作成

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -9,6 +9,14 @@ class TodoController extends Controller
 {
     public function index()
     {
-        return view('index');
+        $todos = Todo::all();
+        return view('index', compact('todos'));
     }
+
+    public function store(Request $request)
+    {
+        $todo = $request->only(['content']);
+        Todo::create($todo);
+        return redirect('/');
+        }
 }
