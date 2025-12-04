e05dc4042b0e4d4740bd658d9414a08b22eea307
ikemi yuki <miilyu237@outlook.jp>
2025-12-04 16:00:06 +0900

    Todoの追加、検索機能の実装

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,6 +16,7 @@ use App\Http\Controllers\CategoryController;
 */
 
 Route::get('/', [TodoController::class, 'index']);
+Route::get('/todos/search', [TodoController::class, 'search']);
 Route::post('/todos', [TodoController::class, 'store']);
 Route::patch('/todos/update', [TodoController::class, 'update']);
 Route::delete('/todos/delete', [TodoController::class, 'destroy']);
@@ -24,3 +25,4 @@ Route::get('/categories', [CategoryController::class, 'index']);
 Route::post('/categories', [CategoryController::class, 'store']);
 Route::patch('/categories/update', [CategoryController::class, 'update']);
 Route::delete('/categories/delete', [CategoryController::class, 'destroy']);
+
